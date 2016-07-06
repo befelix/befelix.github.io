cbe361a4a236b2b46fa9bbf1df8db007ed32f94c
Felix Berkenkamp <befelix@ethz.ch>
2016-07-06 19:53:09 +0200

    Better material handling

--- javascript/toggle.js
+++ javascript/toggle.js
@@ -3,3 +3,33 @@ function toggleElement(element) {
      $(document).ready($(element).slideToggle("fast"));
 }
 
+function toggleMaterial (key, element) {
+
+  var new_element = "." + key + "-" + element
+
+  // If current one is visible it needs to go away
+  if ($(new_element).is(":visible")){
+    $("." + key + "-" + element).slideToggle('fast')
+  }
+  else{
+    var active_element = ""
+
+    // Get the element that is currently visible
+    $("." + key + "-materials").children().each(function (index) {
+      if ($(this).is(":visible")){
+        active_element += $(this).attr("class");
+        return false;
+      }
+    })
+
+    // if it exists, hide current one and show new one
+    if (active_element){
+      $("." + active_element).hide()
+      $(new_element).show()
+    }
+    else{
+      // None are visible. show new one
+      $(new_element).slideToggle('fast')
+    }
+  }
+}
