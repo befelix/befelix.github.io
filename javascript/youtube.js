80d40837e7fd644dce25bddcd256efa75ddd3863
Felix Berkenkamp <fberkenkamp@gmail.com>
2016-07-12 18:57:20 +0200

    Resize videos adaptively

--- javascript/youtube.js
+++ javascript/youtube.js
@@ -1,19 +1,24 @@
 $(document).ready(function () {
 
+  function resize(element) {
+
+    var height = element.height();
+    var width = element.width();
+
+    // Scale down/up div size to fit surrounding container
+    var new_width = element.parent().width();
+    var new_height = height * new_width / width;
+
+    // Set css for element
+    element.css({"width": new_width, "height": new_height});
+  }
+
   var videos = $("div.youtube");
 
   // Rescale youtubes to fit page width
   videos.each(function() {
     // Current width/height of the enclosing div
-    var height = $(this).height();
-    var width = $(this).width();
-
-    // Scale down/up div size to fit surrounding container
-    var new_width = $(this).parent().width();
-    var new_height = height * new_width / width;
-
-    // Set css for element
-    $(this).css({"width": new_width + 'px', "height": new_height + 'px'});
+    resize($(this))
 
     // Replace div with thumbs
     $(this).append('<img class="thumb" src="http://i.ytimg.com/vi/' + $(this).attr("id") + '/hqdefault.jpg"></img>');
@@ -34,7 +39,11 @@ $(document).ready(function () {
     // Fit the video to the surrounding element (div)
     parent.fitVids();
   });
-});
 
-// $(window).resize(function () {
-// });
+  // Make sure elements stay correct size afte resizing
+  $(window).resize(function () {
+    $("div.youtube").each(function() {
+      resize($(this))
+    });
+  });
+});
