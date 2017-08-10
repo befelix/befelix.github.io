be7a6d7a24cf73e5ebc4db71669f154b8a380efe
Felix Berkenkamp <fberkenkamp@gmail.com>
2017-08-10 11:29:41 -0400

    Video tabs working

--- _includes/js/publications.js
+++ _includes/js/publications.js
@@ -12,28 +12,11 @@ $(hash).addClass("active");
 $("a.jupyter").removeAttr("href");
 
 // Highlight the element that we've nacigated too
-var hash = window.location.hash;
 $(hash).parent().css("background-color", "#e6e6ff");
 
 $(document).ready(function(){
-  // code for closing tabs
-  $('div.reference a.nav-link').click(function (e) {
-    var tab = $(this);
-    if(tab.hasClass('active')){
-      window.setTimeout(function(){
-        $(tab.attr('href')).removeClass('active')
-        tab.removeClass('active')
-      }, 1);
-    }
-  });
 
-  // add code for close button on mobile
-  $('a.duplicate').click(function () {
-    var parent = $(this).attr('dublicate-target')
-    $(parent).trigger('click')
-  })
-
-  // Make video point to tab instead
+  // Make video point to tab instead and add information
   $('a.video-tab').each(function () {
     var link = $(this);
     var video_url = link.attr('href');
@@ -44,17 +27,44 @@ $(document).ready(function(){
 
     link.attr('data-toggle', 'tab');
     link.attr('href', target);
-
-    // load / unload video as desired
-    link.click(function() {
-      var iframe = $(target).children('iframe');
-      if (link.hasClass('active')) {
-        iframe.remove();
-      } else {
-        $(target).append("<iframe class='embed-responsive-item' width='560' height='315' src='https://www.youtube.com/embed/"
-                         + video_id
-                         + "?autoplay=1' frameborder='0' allowfullscreen></iframe>");
-      };
-    });
+    link.attr('video-id', video_id)
   });
+
+  // handle the closing of active tabs
+  $('div.reference a.nav-link').click(function (e) {
+    var link = $(this);
+
+    // Make sure that the current video stops playing
+    if (link.is('[data-toggle]')) {
+      // if we're switching to another tab -- remove the video.
+      link.closest('div.reference').find('iframe').remove();
+    } else if (link.is('[target]')) {
+      // if we're going to an external website, let's untoggle any
+      // active video tab
+      // TODO: Pausing or reloading without autoplay would be better
+      link.closest('div.reference').find('a.active.video-tab').trigger('click');
+    }
+
+    // Remove the active field (allow closing of open tabs)
+    if (link.hasClass('active')) {
+      // timeout to allow bootstrap code to finish
+      window.setTimeout(function(){
+        $(link.attr('href')).removeClass('active')
+        link.removeClass('active')
+      }, 1);
+    } else if (link.hasClass('video-tab')) {
+      // Add the current video
+      var target = link.attr('href');
+      var video_id = link.attr('video-id');
+      $(target).append("<iframe class='embed-responsive-item' width='560' height='315' src='https://www.youtube.com/embed/"
+                       + video_id
+                       + "?autoplay=1' frameborder='0' allowfullscreen></iframe>");
+    };
+  });
+
+  // add code for close button on mobile
+  $('a.duplicate').click(function () {
+    var parent = $(this).attr('dublicate-target')
+    $(parent).trigger('click')
+  })
 });
