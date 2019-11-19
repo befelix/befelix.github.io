a0c05554173f4fdd9c6ff1866a840d3a854313cf
Felix Berkenkamp <fberkenkamp@gmail.com>
2019-11-19 22:59:23 +0100

    Update job information

--- _includes/js/contact.js
+++ _includes/js/contact.js
@@ -1,11 +1,11 @@
 // Hide actual email adress
 var m_ = "mailto:";
 var a_ = "@";
-var name = "befelix";
-var dom = "inf.ethz.ch";
+var name = "berkenkamp";
+var dom = "gmail.com";
 var s = name + a_ + dom;
-$("a.email").attr("href", m_ + s).text(s)
+$("a.email").attr("href", m_ + 'f' + s).text('f' + s);
 
 // Display skype name
-var skype = "felix.ber"
-$("a.skype").attr("href", "skype:" + skype).text("Skype")
+var skype = "felix.ber";
+$("a.skype").attr("href", "skype:" + skype).text("Skype");
