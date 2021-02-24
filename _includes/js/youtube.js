7a1301df0df881c8841362baa3283fac6e5c1eb3
Felix Berkenkamp <fberkenkamp@gmail.com>
2021-02-24 21:49:58 +0100

    Simplify video sizing

--- _includes/js/youtube.js
+++ _includes/js/youtube.js
@@ -1,30 +1,3 @@
-function resize(element) {
-
-  var height = element.height();
-  var width = element.width();
-
-  // Scale down/up div size to fit surrounding container
-  var new_width = element.parents('div').width();
-  var new_height = height * new_width / width;
-
-  // Set css for element
-  element.css({"width": new_width, "height": new_height});
-}
-
-var all_videos = $("div.video");
-
-// Rescale youtubes to fit page width
-all_videos.each(function() {
-  resize($(this))
-});
-
-// Make sure elements stay correct size afte resizing
-$(window).resize(function () {
-    $("div.video").each(function() {
-        resize($(this))
-    });
-});
-
 // Disable video link
 $("a.videolink").click(function() { return false; });
 
