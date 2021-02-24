248015b675c61bdd952694ada3067e3a5812d67e
Felix Berkenkamp <fberkenkamp@gmail.com>
2021-02-24 22:56:34 +0100

    Replace video jquery by plain js

--- _includes/js/youtube.js
+++ _includes/js/youtube.js
@@ -1,32 +1,47 @@
 // Disable video link
-$("a.videolink").click(function() { return false; });
+document.querySelectorAll("a.videolink").forEach(function(link) {
+  link.removeAttribute("href");
+})
 
-// Add click to the youtube video
-$("div.video.youtube").click(function () {
+// Replace a video placeholder by an iframe
+function replaceVideoPlaceholder(placeholder, url) {
+  // Create new video iframe
+  var iframe = document.createElement("iframe");
+  iframe.classList.add("embed-responsive-item");
+  iframe.setAttribute("width", "560");
+  iframe.setAttribute("height", "315");
+  iframe.setAttribute("src", url);
+  iframe.setAttribute("frameborder", "0");
+  iframe.setAttribute("allowfullscreen", "true");
+  
+  // The parent container now embeds a responsive object
+  container = placeholder.parentElement;
+  container.classList.add("embed-responsive");
+  container.classList.add("embed-responsive-16by9");
+  
+  // Replace the placeholder with the iframe
+  placeholder.replaceWith(iframe);
+}
 
-  var placeholder = $(this);
-  var div = placeholder.parent();
+// Add click to the Youtube videos
+document.querySelectorAll("div.video.youtube").forEach(function(placeholder) {
+  placeholder.addEventListener("click", function() {
+      var url = "https://www.youtube.com/embed/" + this.id + "?autoplay=1";
 
-  var url = "https://www.youtube.com/embed/" + this.id + "?autoplay=1";
+      // Add starttime if defined
+      var starttime = placeholder.getAttribute('start');
+      if (typeof starttime !== 'undefined') {
+          url += "&start=" + starttime;
+      }
 
-  // Add starttime if defined
-  var starttime = placeholder.attr('start');
-  if (typeof starttime !== 'undefined') {
-      url += "&start=" + starttime;
-  }
-
-  placeholder.replaceWith("<iframe class='embed-responsive-item' width='560' height='315' src='" + url + "' frameborder='0' allowfullscreen></iframe>");
-
-  div.addClass('embed-responsive embed-responsive-16by9');
-});
+      replaceVideoPlaceholder(placeholder=this, url=url)
+  })
+})
 
 
 // Add click to the ETH videos
-$("div.video.eth").click(function () {
-
-    var placeholder = $(this);
-    var div = placeholder.parent();
-
-    placeholder.replaceWith("<iframe class='embed-responsive-item' width='560' height='315' src='" + this.id + "' frameborder='0' allowfullscreen></iframe>");
-    div.addClass('embed-responsive embed-responsive-16by9');
-});
+document.querySelectorAll("div.video.eth").forEach(function(placeholder) {
+  placeholder.addEventListener("click", function() {
+      replaceVideoPlaceholder(placeholder=this, url=this.id)
+  })
+})
